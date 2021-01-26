02a8fe570c9423ab43c635e1d8a12f80e8023ba5
Michael Haslam <ongomobile@gmail.com>
2021-01-26 14:58:21 -0800

    Initial Commit

---
+++ SendEmailOnButtonTap/SendEmailOnButtonTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendEmailOnButtonTapApp.swift
+//  SendEmailOnButtonTap
+//
+//  Created by Michael Haslam on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SendEmailOnButtonTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
